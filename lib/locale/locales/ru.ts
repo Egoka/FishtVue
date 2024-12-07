daf00d3a029db25570d1129185c4e4332c132041
Egoka <eg.bonda2014@gmail.com>
2024-12-07 16:25:33 +0300

    fix(utils): utilities, locales, and theme have been fixed and updated

--- lib/locale/locales/ru.ts
+++ lib/locale/locales/ru.ts
@@ -1,10 +1,19 @@
 import type { DefaultMessages } from "fishtvue/locale"
 
 export default <DefaultMessages>{
+  of: "из",
+  items: "сущностей",
+  lines: "строк",
+  previous: "Предыдущий",
+  next: "Следующий",
+  save: "Сохранить",
+  increase: "Увеличить",
+  show: "Показать:",
   find: "Найти...",
   copy: "Копировать",
   requiredField: "Обязательное поле",
   noData: "Нет данных",
   noColumn: "Нет столбцов",
-  noDataForQuery: "По вашему запросу не найдено данных"
+  noDataForQuery: "По вашему запросу не найдено данных",
+  clearAllFilters: "Очистить все фильтры"
 }
